a431048a444ba16343bcc1837fe25bd102c5e6a6
Ben Krebsbach <bkrebsbach@atlassian.com>
2019-07-29 10:55:21 -0500

    Update README and add comment for flag parsing

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"flag"
+	"fmt"
 	"os"
 
 	producer "github.com/asecurityteam/component-producer"
@@ -82,6 +82,8 @@ func main() {
 	runner := new(func(context.Context, settings.Source) error)
 	cmp := newComponent()
 
+	// Print names and example values for all defined environment variables
+	// when -h or -help are passed as flags.
 	fs := flag.NewFlagSet("nexpose-asset-attributor", flag.ContinueOnError)
 	fs.Usage = func() {}
 	if err = fs.Parse(os.Args[1:]); err == flag.ErrHelp {
