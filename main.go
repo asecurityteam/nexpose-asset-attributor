1c171e25585f61bdf85ca5a3bb12e2b9138ae393
Brandon Chan <bchan@atlassian.com>
2019-09-25 16:38:12 -0500

    added noop assetvalidator to main.go

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	producer "github.com/asecurityteam/component-producer"
 	"github.com/asecurityteam/nexpose-asset-attributor/pkg/assetattributionfailure"
 	"github.com/asecurityteam/nexpose-asset-attributor/pkg/assetattributor"
+	"github.com/asecurityteam/nexpose-asset-attributor/pkg/assetvalidator"
 	"github.com/asecurityteam/nexpose-asset-attributor/pkg/domain"
 	v1 "github.com/asecurityteam/nexpose-asset-attributor/pkg/handlers/v1"
 	"github.com/asecurityteam/serverfull"
@@ -58,8 +59,9 @@ func (c *component) New(ctx context.Context, conf *config) (func(context.Context
 		LogFn:           domain.LoggerFromContext,
 		StatFn:          domain.StatFromContext,
 		AssetAttributor: a,
-		// AttributionFailureHandler field is simply a stub, it varies org to org
+		// AttributionFailureHandler and AttributedAssetValidator fields are stubs, it varies company to company
 		AttributionFailureHandler: &assetattributionfailure.NoopAttributionFailureHandler{},
+		AttributedAssetValidator:  &assetvalidator.NoopAttributtedAssetValidator{},
 		Producer:                  p,
 	}
 	handlers := map[string]serverfull.Function{
