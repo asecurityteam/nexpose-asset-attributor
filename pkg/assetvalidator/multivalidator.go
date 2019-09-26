6e4c036f085e597ff31df0c6a92106b7bed81f4d
Brandon Chan <bchan@atlassian.com>
2019-09-26 13:46:14 -0500

    changed multivalidator field to capital

--- pkg/assetvalidator/multivalidator.go
+++ pkg/assetvalidator/multivalidator.go
@@ -10,16 +10,16 @@ import (
 // of type AssetValidator. In the event that a company needs different validation checks for an attributed asset, this implementation
 // will handle that
 type MultiAttributedAssetValidator struct {
-	validators []domain.AssetValidator
+	Validators []domain.AssetValidator
 }
 
 // Validate is an implementation that will run multiple validations in a fan out pattern.
 // If any validator fails, then the entire multi validation fails
 func (v *MultiAttributedAssetValidator) Validate(ctx context.Context, attributedAsset domain.NexposeAttributedAssetVulnerabilities) error {
 
-	validationResults := make(chan error, len(v.validators))
+	validationResults := make(chan error, len(v.Validators))
 
-	for _, validationMethod := range v.validators {
+	for _, validationMethod := range v.Validators {
 		go func(validator domain.AssetValidator, ctx context.Context, attributedAsset domain.NexposeAttributedAssetVulnerabilities) {
 			// send back nil or error from Validate, will check results later
 			validationResults <- validator.Validate(ctx, attributedAsset)
@@ -28,7 +28,7 @@ func (v *MultiAttributedAssetValidator) Validate(ctx context.Context, attributed
 
 	var errorList []error
 
-	for range v.validators {
+	for range v.Validators {
 		err := <-validationResults
 		if err != nil {
 			errorList = append(errorList, err)
