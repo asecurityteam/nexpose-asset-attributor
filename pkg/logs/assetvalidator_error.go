6de06b236358c1286312413a7721549f16aab012
Alex Slape <aslape@c02c316mlvdn.office.atlassian.com>
2020-02-24 08:32:10 -0600

    Log ARN and resourcetype info for assets that fail validation.

--- pkg/logs/assetvalidator_error.go
+++ pkg/logs/assetvalidator_error.go
@@ -5,9 +5,11 @@ import "github.com/asecurityteam/nexpose-asset-attributor/pkg/domain"
 // AssetValidationFailure occurs when an asset was found in an asset inventory system
 // and subsequent validation of that asset completed, but with a failure result.
 type AssetValidationFailure struct {
-	Message string `logevent:"message,default=validation-failure"`
-	Reason  string `logevent:"reason,default=unknown-validation-failure"`
-	AssetID int64  `logevent:"assetID,default=id-not-specified"`
+	Message      string `logevent:"message,default=validation-failure"`
+	Reason       string `logevent:"reason,default=unknown-validation-failure"`
+	AssetID      int64  `logevent:"assetID,default=id-not-specified"`
+	ARN          string `logevent:"arn,default=arn-not-specified"`
+	ResourceType string `logevent:"resourcetype,default=unknown-resourcetype"`
 }
 
 // AssetValidationError occurs when an asset was found in an asset inventory system
@@ -20,10 +22,10 @@ type AssetValidationError struct {
 
 // ValidationErrorLogFactory is a factory function that takes an error that occurs during validation,
 // and returns a corresponding struct with logging information
-func ValidationErrorLogFactory(validationErr error, assetID int64) interface{} {
+func ValidationErrorLogFactory(validationErr error, assetID int64, arn string, resourceType string) interface{} {
 	switch validationErr.(type) {
 	case domain.ValidationFailure:
-		return AssetValidationFailure{Reason: validationErr.Error(), AssetID: assetID}
+		return AssetValidationFailure{Reason: validationErr.Error(), AssetID: assetID, ARN: arn, ResourceType: resourceType}
 	default:
 		return AssetValidationError{Reason: validationErr.Error(), AssetID: assetID}
 	}
